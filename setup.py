57ba39966478e86d2351a02da94c012d5cb97754
Jonas Köhler <jonas.koehler.ks@gmail.com>
2022-11-08 18:12:03 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="flox",
+    version="0.1.0",
+    url="https://github.com/noegroup/flox.git",
+    author="Jonas Köhler",
+    author_email="jonas.koehler.ks@gmail.com",
+    description="flox - flows in jax - sampling focused normalizing flow lib",
+    packages=find_packages(),
+    install_requires=[
+        "jax >= 0.3.24",
+        "jax-dataclasses >= 1.4.4",
+        "jaxlib >= 0.3.24",
+        "jaxopt >= 0.5.5",
+        "jaxtyping >= 0.2.7",
+        "optax >= 0.1.3",
+        "python-lenses >= 1.1.0",
+        "numpyro >= 0.10.1",
+        "dm-haiku >= 0.0.8",
+        "distrax >= 0.1.2",
+    ],
+)
